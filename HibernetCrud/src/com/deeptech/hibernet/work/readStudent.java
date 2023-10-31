526e73a6d4a89208b5b2f2d5108d4a44d276d7a0
DhanushArjunan79 <149300800+DhanushArjunan79@users.noreply.github.com>
2023-10-31 21:30:10 +0530

    Update readStudent.java

--- HibernetCrud/src/com/deeptech/hibernet/work/readStudent.java
+++ HibernetCrud/src/com/deeptech/hibernet/work/readStudent.java
@@ -1,4 +1,4 @@
-package com.deeptech.hibernate.wrok;
+package com.deeptech.hibernate.work;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -7,8 +7,8 @@ import java.util.List;
 
 import org.hibernate.Query;
 
-import com.deeptech.hibernate.wrok.dto.Student;
-import com.deeptech.hibernate.wrok.utility.HibernateUtil;
+import com.deeptech.hibernate.work.dto.Student;
+import com.deeptech.hibernate.work.utility.HibernateUtil;
 
 public class readStudent 
 {
@@ -23,7 +23,7 @@ public class readStudent
 		List<Student>st=q.list();
 		for(Student s :st)
 		{
-			System.out.println(s.getSudentName() +"\t"+s.getRollNumber()+"\t"+s.getCourse());
+			System.out.println(s.getStudentName() +"\t"+s.getRollNumber()+"\t"+s.getCourse());
 		}
         ses.getTransaction().commit();
 	}
