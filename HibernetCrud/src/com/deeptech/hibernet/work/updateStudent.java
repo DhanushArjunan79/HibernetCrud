b4fc6c940738aa6323c01864be4f2e761b3a7c20
DhanushArjunan79 <149300800+DhanushArjunan79@users.noreply.github.com>
2023-10-31 18:11:31 +0530

    Update updateStudent.java

--- HibernetCrud/src/com/deeptech/hibernet/work/updateStudent.java
+++ HibernetCrud/src/com/deeptech/hibernet/work/updateStudent.java
@@ -1,9 +1,25 @@
-package com.deeptech.hibernet.work;
+package com.deeptech.hibernate.wrok;
 
-public class updateStudent {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import com.deeptech.hibernate.wrok.dto.Student;
+import com.deeptech.hibernate.wrok.utility.HibernateUtil;
+
+public class UpdateStudent 
+{
+	public static void main(String[] args) 
+	{
+		SessionFactory sf=HibernateUtil.connection();
+		Session ses=sf.openSession();
+		ses.beginTransaction();
+		
+		Student s= ses.get(Student.class, 1);
+		s.setSudentName("Monisha");
+		ses.getTransaction().commit();
+		ses.close();
+		sf.close();
+		System.out.println("update success");
 
 	}
 
